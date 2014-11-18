60cf1f264523126f89ee0b5f1087ac38caaf6b67
rfosterslo <rfosterslo@gmail.com>
2014-11-18 19:47:22 +0000

    Fixed admin_urls.py

--- wagtailplus/wagtailcontacts/admin_urls.py
+++ wagtailplus/wagtailcontacts/admin_urls.py
@@ -4,6 +4,7 @@ Contains admin URLs.
 from django.conf.urls import url
 from django.contrib.auth.decorators import permission_required
 
+from wagtailplus.view import chooser
 from wagtailplus.views import crud
 
 from .models import Contact
@@ -45,5 +46,33 @@ urlpatterns = [
             success_url='wagtailcontacts_index'
         )),
         name='wagtailcontacts_delete_contact'
+    ),
+    # Contact chooser URLs.
+    url(
+        r'^chooser/$',
+        chooser.ChooseView.as_view(
+            model=Contact,
+            results_template='wagtailcontacts/chooser/results.html',
+            chooser_javascript='wagtailcontacts/chooser/chooser.js'
+        ),
+        name='wagtailcontacts_contact_chooser'
+    ),
+    url(
+        r'^chooser/create/$',
+        chooser.CreateView.as_view(
+            model=Contact,
+            chooser_template='wagtailcontacts/chooser/results.html',
+            chooser_javascript='wagtailcontacts/chooser/chooser.js',
+            chosen_javascript='wagtailcontacts/chooser/chosen.js'
+        ),
+        name='wagtailcontacts_contact_chooser_create'
+    ),
+    url(
+        r'^chooser/(?P<pk>\d+)/$',
+        chooser.ChosenView.as_view(
+            model=Contact,
+            chosen_javascript='wagtailcontacts/chooser/chosen.js'
+        ),
+        name='wagtailcontacts_contact_chosen'
     ),
 ]
