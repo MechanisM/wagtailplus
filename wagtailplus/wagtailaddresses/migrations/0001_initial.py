096143fc2f4659f4ee9d63126fe30882950a6f59
rfosterslo <rfosterslo@gmail.com>
2015-04-17 00:10:01 +0000

    Reset migrations

--- wagtailplus/wagtailaddresses/migrations/0001_initial.py
+++ wagtailplus/wagtailaddresses/migrations/0001_initial.py
@@ -2,95 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import wagtail.wagtailadmin.taggable
-import django.contrib.gis.db.models.fields
-import taggit.managers
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('taggit', '0001_initial'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Address',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('geom', django.contrib.gis.db.models.fields.PointField(default=b'', verbose_name='Geometry', srid=4326, editable=False, blank=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created')),
-                ('label', models.CharField(verbose_name='Label', unique=True, max_length=200, editable=False)),
-                ('street_number', models.CharField(max_length=6, verbose_name='Street Number', db_index=True)),
-            ],
-            options={
-                'verbose_name': 'Address',
-                'verbose_name_plural': 'Addresses',
-            },
-            bases=(models.Model, wagtail.wagtailadmin.taggable.TagSearchable),
-        ),
-        migrations.CreateModel(
-            name='AddressComponent',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('type', models.CharField(max_length=50, verbose_name='Component Type', db_index=True)),
-                ('short_name', models.CharField(max_length=255, verbose_name='Short Name', db_index=True)),
-                ('long_name', models.CharField(max_length=255, verbose_name='Long Name', db_index=True)),
-                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text=None, verbose_name='Tags')),
-            ],
-            options={
-                'ordering': ('long_name',),
-            },
-            bases=(models.Model, wagtail.wagtailadmin.taggable.TagSearchable),
-        ),
-        migrations.AlterUniqueTogether(
-            name='addresscomponent',
-            unique_together=set([('type', 'long_name'), ('type', 'short_name')]),
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='administrative_area_level_1',
-            field=models.ForeignKey(related_name='+', verbose_name='State', to='wagtailaddresses.AddressComponent'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='administrative_area_level_2',
-            field=models.ForeignKey(related_name='+', verbose_name='County', blank=True, to='wagtailaddresses.AddressComponent', null=True),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='country',
-            field=models.ForeignKey(related_name='+', verbose_name='Country', to='wagtailaddresses.AddressComponent'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='locality',
-            field=models.ForeignKey(related_name='+', verbose_name='City', to='wagtailaddresses.AddressComponent'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='postal_code',
-            field=models.ForeignKey(related_name='+', verbose_name='Postal Code', to='wagtailaddresses.AddressComponent'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='route',
-            field=models.ForeignKey(related_name='+', verbose_name='Street Name', to='wagtailaddresses.AddressComponent'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='tags',
-            field=taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text=None, verbose_name='Tags'),
-            preserve_default=True,
-        ),
-        migrations.AlterUniqueTogether(
-            name='address',
-            unique_together=set([('street_number', 'route', 'postal_code')]),
-        ),
     ]
--- wagtailplus/wagtailcontacts/migrations/0001_initial.py
+++ wagtailplus/wagtailcontacts/migrations/0001_initial.py
@@ -2,43 +2,12 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import localflavor.us.models
-import django.db.models.deletion
-import taggit.managers
-import wagtail.wagtailadmin.taggable
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailimages', '0005_make_filter_spec_unique'),
-        ('taggit', '0001_initial'),
-        ('wagtailaddresses', '0001_initial'),
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Contact',
-            fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('name', models.CharField(help_text='Enter a name for this contact', max_length=100, verbose_name='Name', db_index=True)),
-                ('website', models.URLField(help_text='Enter an optional website for this contact', null=True, verbose_name='Website', blank=True)),
-                ('email', models.EmailField(help_text='Enter an optional email address for this contact', max_length=75, null=True, verbose_name='Email', blank=True)),
-                ('telephone', localflavor.us.models.PhoneNumberField(help_text='Enter an optional telephone number for this contact', max_length=20, null=True, verbose_name='Telephone', blank=True)),
-                ('address', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailaddresses.Address', help_text='Select an optional address for this contact', null=True)),
-                ('image', models.ForeignKey(related_name='+', on_delete=django.db.models.deletion.SET_NULL, blank=True, to='wagtailimages.Image', help_text='Select an optional image for this contact', null=True)),
-                ('tags', taggit.managers.TaggableManager(to='taggit.Tag', through='taggit.TaggedItem', blank=True, help_text=None, verbose_name='Tags')),
-            ],
-            options={
-                'ordering': ('name',),
-                'verbose_name': 'Contact',
-                'verbose_name_plural': 'Contacts',
-            },
-            bases=(models.Model, wagtail.wagtailadmin.taggable.TagSearchable),
-        ),
-        migrations.AlterUniqueTogether(
-            name='contact',
-            unique_together=set([('name', 'email'), ('name', 'website')]),
-        ),
     ]
